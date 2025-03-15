cbfa4f4a25928c483437b225bab9bd98352c91e8
eJwtzLtuAyEQQNF/mZpi3/bSRikjRYldpEIDDA5edkHD4kcs/3ska9srnfsANIZyPsSJFpDgrOuN2Rm0Y6ebVlfWkhtHJN26fdc7aqsdVm0FAhLHi7fEG31sJ7Vuq9NvVBf8mofUN+VWzLk7vv99xjfy4UcPtdl/Nx/DWNcg4EXUek8EEjQhE4OAbOIroJ39IlPRwRs10V0woZWRT4IpRVEysaQZfRDXyJML8QoC6JY8U1a4glxKCM/nPz6nUP8= <faridamahmoud@std.mans.edu.eg>
2025-03-15 02:19:45 +0200

    Add Project Files

---
+++ HelthCareProject/Program.cs
@@ -0,0 +1,45 @@
+using HelthCareProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+
+namespace HelthCareProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            #region Connect To MySQL DB
+            var ConnectionString = builder.Configuration.GetConnectionString("mySql");
+            builder.Services.AddDbContext<helthcare_systemContext>(options =>
+                options.UseMySql(ConnectionString, ServerVersion.Parse("8.0.41-mysql")));
+            #endregion
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
